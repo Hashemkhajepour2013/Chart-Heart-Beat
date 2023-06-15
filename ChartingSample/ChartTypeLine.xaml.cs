db26f0886555452a77482f833dd08f8f2eca28a3
123@qwe <hashemkhajepour@yahoo.com>
2023-06-15 20:52:57 +0330

    chart heart beat

---
+++ ChartingSample/ChartTypeLine.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Controls.Primitives;
+
+namespace ChartingSample
+{
+    public partial class ChartTypeLine : Window
+    {
+        ObservableCollection<Data> generatedData = new ObservableCollection<Data>();
+
+        public ChartTypeLine()
+        {
+            InitializeComponent();
+            generatedData = GenerateData();
+            LoadLineChartData(0);
+        }
+
+        private new ObservableCollection<Data> GenerateData()
+        {            
+            Random rnd = new Random();
+
+            for (int i = 0; i < 500; i++)
+            {
+                generatedData.Add(new Data
+                {
+                    Time = (double)i,
+                    Value = rnd.Next(-2, 2)
+                });
+            }
+
+            return generatedData;
+        }
+
+        private void LoadLineChartData(int scroll)
+        {        
+
+
+            List<KeyValuePair<double, double>> d = new List<KeyValuePair<double, double>>();
+            int oldscroll = scroll + 50;
+                for (; scroll <= oldscroll; scroll++)
+                {
+                    d.Add(new KeyValuePair<double, double>(generatedData[scroll].Time, generatedData[scroll].Value));
+                }
+
+            ((LineSeries)mcChart.Series[0]).ItemsSource = d;
+        }
+
+        private void scroll_Scroll(object sender, ScrollEventArgs e)
+        {
+            var scroll = (int)e.NewValue;
+            LoadLineChartData(scroll);
+        }
+    }
+
+    public class Data
+    {
+        public double Time { get; set; }
+
+        public double Value { get; set; }
+    }
+}
---
+++ ChartingSample/Window1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+
+namespace ChartingSample
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void LineChartButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChartTypeLine lineChart = new ChartTypeLine();
+            lineChart.Owner = this;
+            lineChart.Show();
+        }
+    }
+}
